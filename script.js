af0d303476c6333501de7cf17331f106c4719125
neducod <nedunony16@gmail.com>
2025-12-24 09:46:14 +0100

    Adding Javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const timer = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
+
+
+function startTimer(){
+    document.getElementById("timer").innerHTML = setInterval();
+}
